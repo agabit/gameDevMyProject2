44f8fab431049f986afcdb8fd7553973e93b8b75
agabit <gabit.aidarbek@gmail.com>
2023-01-13 18:08:12 +0500

    init test test

---
+++ MyProject2/Source/MyProject2/Private/test.cpp
@@ -0,0 +1,29 @@
+﻿// Gabit
+
+
+#include "test.h"
+
+// Sets default values
+Atest::Atest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void Atest::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Сәлем Әлем!"));
+	UE_LOG(LogTemp, Warning, TEXT("Hello World!"));
+	UE_LOG(LogTemp, Warning, TEXT("Привет Мир!"));	
+}
+
+// Called every frame
+void Atest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
